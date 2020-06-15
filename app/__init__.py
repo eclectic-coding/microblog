71c86a540ff2d06854ea52b026b009d819e4e274
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-06-15 14:45:01 -0400

    Configure time stamps

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from logging.handlers import SMTPHandler
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from flask_moment import Moment
+
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
