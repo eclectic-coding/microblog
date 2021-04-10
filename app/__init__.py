eda9977c046cfb48320022fb1a4e10298847d336
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-04-10 18:04:55 -0400

    Set up User model

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
-from app import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app import routes, models
 
