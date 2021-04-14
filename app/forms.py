3b9377983b526fb503b00d17ab9677443d2f042e
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-04-14 16:50:38 -0400

    Add unit tests for models

--- app/forms.py
+++ app/forms.py
@@ -34,3 +34,4 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
